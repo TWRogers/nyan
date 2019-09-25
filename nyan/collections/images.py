29fc25c86c7a904669ee5b8b3e1ecdf02e93a316
tom <tom@visulytix.com>
2019-09-25 15:37:32 +0100

    added _inverse functions and image mapping

--- nyan/collections/images.py
+++ nyan/collections/images.py
@@ -1,8 +1,8 @@
 import typing
 import numpy as np
-from nyan import History
+from nyan.utils import History
 import cv2
-
+import matplotlib.pyplot as plt
 
 COLOUR_MODES = ('RGB', 'BGR')
 
@@ -64,7 +64,7 @@ class Images(object):
         return np.array(self.images)
 
     @property
-    def shape(self) -> tuple:
+    def shape(self) -> tuple or None:
         if self.images:
             shapes = list(set(map(lambda x: x.shape, self.images)))
             if len(shapes) == 1:
@@ -75,7 +75,7 @@ class Images(object):
             return None
 
     @property
-    def size(self) -> tuple:
+    def size(self) -> tuple or None:
         if self.images:
             sizes = list(set(map(lambda x: x.shape[:2][::-1], self.images)))
             if len(sizes) == 1:
@@ -129,8 +129,8 @@ class Images(object):
 
         new_copy.channel_mode = self.channel_mode
         new_copy.debug_mode = self.debug_mode
-        new_copy.images = self.images.copy()
-        new_copy._original_images = self._original_images.copy()
+        new_copy.images = [im.copy() for im in self.images]
+        new_copy._original_images = [im.copy() for im in self._original_images]
         new_copy._transform_history = self._transform_history.copy()
         new_copy._debug_history = self._debug_history.copy()
 
@@ -141,11 +141,20 @@ class Images(object):
 
     def transform_image(self, image, start_event: str = 'start', end_event: str = 'end') -> np.ndarray:
         relevant_transform_history, direction = self._get_relevant_history(start_event, end_event)
-        raise NotImplementedError
-        #
-        # if direction:
-        #     for event in relevant_transform_history:
-        #         if event['fn_name'] == '':
+
+        if direction:
+            for event in relevant_transform_history:
+                getattr(image, event['fn_name'])(*event['fn_args']['args'],
+                                                 **event['fn_args']['kwargs'])
+        else:
+            for event in relevant_transform_history[::-1]:
+                try:
+                    getattr(image, '{}_inverse'.format(event['fn_name']))(*event['fn_args']['args'],
+                                                                          **event['fn_args']['kwargs'])
+                except AttributeError:
+                    raise NotImplementedError('Inverse fn ({}_inverse) '
+                                              'for {} is not Implemented'.format(event['fn_name'], event['fn_name']))
+        return image
 
     def transform_point(self, point, start_event, end_event) -> tuple:
         raise NotImplementedError
@@ -162,7 +171,7 @@ class Images(object):
         if start_idx < end_idx:
             return self._transform_history[start_idx:end_idx], True
         else:
-            return self._transform_history[end_idx:end_idx], False
+            return self._transform_history[end_idx:start_idx], False
 
     def _get_event_index(self, event_name: str) -> int:
         if event_name not in ('start', 'end'):
@@ -174,8 +183,11 @@ class Images(object):
             return len(self._transform_history)
 
     @History.transform()
-    def rotate(self, angle: float):
+    def _rotate(self, angle: float):
         self.images = [cv2.rotate(image, angle) for image in self.images]
+
+    def _rotate_inverse(self, angle: float):
+        self.images = [cv2.rotate(image, -angle) for image in self.images]
 
     def translate(self, x: int = 0, y: int = 0, pad_colour: typing.Optional[tuple] = None):
 
@@ -191,29 +203,29 @@ class Images(object):
                    y_max=self.size[1] - y if y > 0 else self.size[1])
 
     def crop(self,
-             x_min: typing.Optional[int] = None,
-             x_max: typing.Optional[int] = None,
              y_min: typing.Optional[int] = None,
-             y_max: typing.Optional[int] = None):
+             y_max: typing.Optional[int] = None,
+             x_min: typing.Optional[int] = None,
+             x_max: typing.Optional[int] = None):
 
         left = 0 if x_min is None else x_min
         top = 0 if y_min is None else y_min
         right = 0 if x_max is None else self.size[0]-x_max
         bottom = 0 if y_max is None else self.size[1]-y_max
 
-        self._crop(left, right, top, bottom)
+        self._crop(top, bottom, left, right)
 
     @History.transform()
-    def _crop(self, left: int, right: int, top: int, bottom: int) -> None:
-        self.images = [image[left:-right, top:-bottom] for image in self.images]
+    def _crop(self, top: int, bottom: int, left: int, right: int) -> None:
+        self.images = [image[top:-bottom, left:-right] for image in self.images]
 
-    def _crop_inverse(self):
-        return self._pad
+    def _crop_inverse(self, top: int, bottom: int, left: int, right: int) -> None:
+        return self._pad(top, bottom, left, right, colour=(0, 0, 0))
 
     def resize(self,
                target_size: tuple,
                preserve_aspect_ratio: bool = False) -> None:
-
+        # TODO: implement preserve_aspect_ratio
         if (target_size[0] is None) and (target_size[1] is None):
             fx = 1.
             fy = 1.
@@ -237,7 +249,7 @@ class Images(object):
 
     @History.transform()
     def _resize(self, fx: float, fy: float) -> None:
-        self.images = [cv2.resize(image, fx=fx, fy=fy) for image in self.images]
+        self.images = [cv2.resize(image, dsize=None, fx=fx, fy=fy) for image in self.images]
 
     def _resize_inverse(self, fx: float, fy: float) -> None:
         self._resize(fx=1./fx, fy=1./fy)
@@ -259,18 +271,23 @@ class Images(object):
         self.images = [cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=colour)
                        for image in self.images]
 
-    def _pad_inverse(self):
-        return self._crop
+    def _pad_inverse(self, top, bottom, left, right, colour):
+        return self._crop(top, bottom, left, right)
 
     def label_event(self, label: str) -> None:
         self._transform_history[-1].update({'label': label})
 
+    def show(self, index: int = 0):
+        plt.imshow(self.images[index])
+        plt.show()
+
     @property
     def __array_interface__(self) -> dict:
+        # TODO: implement numpy array interface
         raise NotImplementedError
-    #     # support for casting to a numpy array
-    #     array = self.as_array()
-    #     return {"typestr": array.dtype,
-    #             "shape": self.shape,
-    #             "version": 3,
-    #             "data": array.tobytes()}
+    #   # support for casting to a numpy array
+    #   array = self.as_array()
+    #   return {"typestr": array.dtype,
+    #           "shape": self.shape,
+    #           "version": 3,
+    #           "data": array.tobytes()}
