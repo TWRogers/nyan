56977c2963d280b71c5d71dbe91a32a6c0cec4b1
tom <tom@visulytix.com>
2019-09-26 18:35:41 +0100

    added abstract methods to video

--- nyan/collections/video.py
+++ nyan/collections/video.py
@@ -6,16 +6,19 @@ import typing
 class Video(Images):
 
     def __init__(self,
-                 filepath: typing.Optional[str] = None,
+                 file_path: str = None,
                  channel_mode: str = 'RGB',
                  debug_mode: bool = False) -> None:
 
         super(Video, self).__init__(images=None,
                                     channel_mode=channel_mode,
                                     debug_mode=debug_mode)
-        self.load(filepath)
+        self.load(file_path)
 
-    def load(self, filepath: typing.Optional[str] = None) -> None:
-        if filepath is not None:
-            reader = imageio.get_reader(filepath, 'ffmpeg')
+    def load(self, file_path: typing.Optional[str] = None) -> None:
+        if file_path is not None:
+            reader = imageio.get_reader(file_path, "ffmpeg")
             self.images = [image for image in reader]
+
+    def save(self):
+        raise NotImplementedError
