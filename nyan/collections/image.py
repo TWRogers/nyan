c0411bf6ca8e61a9cd4af45661ef6a31b9c4fb5a
tom <tom@visulytix.com>
2019-09-26 18:19:50 +0100

    added Image class

---
+++ nyan/collections/image.py
@@ -0,0 +1,35 @@
+from .images import Images
+import imageio
+import typing
+import numpy as np
+
+
+class Image(Images):
+
+    def __init__(self,
+                 filepath: typing.Optional[str] = None,
+                 channel_mode: str = 'RGB',
+                 debug_mode: bool = False) -> None:
+
+        super(Image, self).__init__(images=None,
+                                    channel_mode=channel_mode,
+                                    debug_mode=debug_mode)
+        self.load(filepath)
+
+    def load(self, filepath: typing.Optional[str] = None) -> None:
+        if filepath is not None:
+            reader = imageio.get_reader(filepath, 'ffmpeg')
+            self.images = [image for image in reader]
+
+    @property
+    def image(self):
+        return self.images[0]
+
+    def as_array(self) -> np.ndarray:
+        return super(Image, self).as_array()[0, ...]
+
+    def load(self):
+        raise NotImplementedError
+
+    def save(self):
+        raise NotImplementedError
